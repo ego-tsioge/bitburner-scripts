64e54ee9c65943b00327bfa0bd220952ffbacb65
tsioge <{ID}+{username}@users.noreply.github.com>
2023-04-10 22:14:37 +0200

    earlygame strategy & autodeploy
    fokus lag auf basis.js - das script zielt auf foodnstuff und verteilt das tutorial script auf alle hackbaren server.
    
    git-init.js läd alle dateien vom repro ins spiel

--- basis.js
+++ basis.js
@@ -1,4 +1,5 @@
 //basis.js test
+
 /** @param {NS} ns */
 export async function main(ns) {
   function getNumCracks() {
@@ -7,36 +8,25 @@ export async function main(ns) {
       if (ns.fileExists(fileName[0], homeServer)) num++;
     }
     return num;
-  }
-  function getTargetServers() {
-    ns.print("Retrieving all nodes in network");
-    let visited = new Set;
-    let stack = [];
-    let origin = ns.getHostname();
-    let targets = []
-    stack.push(origin);
+  } // end getNumCracks
 
-    while (stack.length > 0) {
-      let node = stack.shift(); //jetzt kein stack mehr
+  function getTargetServers() {
+    let visited = new Set;
+    let speicher = ['home'];
+    let targets = [];
+
+    while (speicher.length > 0) {
+      let node = speicher.shift();
       if (!visited.has(node)) {
         visited.add(node);
         if (canHack(node)) {
           targets.push(node);
           //As a rule of thumb, your hacking target should be the server 
           //with highest max money that’s required hacking level is under 1/3 of your hacking level.
-          if ((ns.getHackingLevel() / 3) > ns.getServerRequiredHackingLevel(node) && ns.getServerMaxMoney(node) > ns.getServerMaxMoney(bestOption)) {
-            ns.tprint("setze bestOption auf ", node);
-            if (node != bestOption) {
-              piggyBank = bestOption;
-              bestOption = node;
-              deployAgain = true;
-            }
-
-          }
         }
         let nachbarn = ns.scan(node);
         for (let child of nachbarn) {
-          stack.push(child);
+          speicher.push(child);
         }
       }
     }
@@ -47,22 +37,24 @@ export async function main(ns) {
       targets.push(serverPrefix + i);
       i++;
     }
-
     return targets;
-  }
+  } // end getTargetServers
+
   function canHack(server) {
     // let numCracks = getNumCracks();
     let reqPorts = ns.getServerNumPortsRequired(server);
     let ramAvail = ns.getServerMaxRam(server);
     //ns.tprint(server," C:",numCracks, " RP:", reqPorts, " RA:",ramAvail);
     return numCracks >= reqPorts && ramAvail >= virusRam;
-  }
+  } // end canHack
+
   async function deployHacks(targets) {
     ns.print("Verteile Virus an Server " + targets);
     for (let server of targets) {
       await copyAndRunVirus(server);
     }
-  }
+  } // end deployHacks
+
   async function copyAndRunVirus(server) {
     ns.print("Copying virus to Server: ", server);
     await ns.scp(virus, server);
@@ -80,7 +72,8 @@ export async function main(ns) {
 
     let maxThreads = Math.floor(ns.getServerMaxRam(server) / virusRam);
     ns.exec(virus, server, maxThreads, bestOption);
-  }
+  } // end copyAndRunVirus
+
   function penetrate(server) {
     ns.print("Penetrating ", server);
     for (let fileName of cracks) {
@@ -88,14 +81,11 @@ export async function main(ns) {
         fileName[1](server);
       }
     }
-  }
-
-  //As a rule of thumb, your hacking target should be the server with highest max money that’s required hacking level is under 1/3 of your hacking level.
+  } // end penetrate
 
   var curBots = [];             // hier sollten die 'Helfer' drin stehen
-  let piggyBank = "n00dles";    // beim Wechsel des Zieles, werden hier das vorherige Ziel aufgeführt
   const waitTime = 100;         // wartezeit für Endlosschleife
-  var bestOption = "n00dles";   // Merker für das beste Ziel
+  var bestOption = "foodnstuff";   // Merker für das  Ziel
   var deployAgain = false;      // bestes Ziel wurde geändert? Virus neu ausrollen
   const virus = "ego_hack.js";              // standard virus
   const virusRam = ns.getScriptRam(virus);  // ram-verbrauch vom standardvirus
@@ -111,6 +101,9 @@ export async function main(ns) {
   ns.tail();
   ns.disableLog('ALL');
   // hauptschleife
+
+  let lifeLine = ['|', '/', '-', '\\'];
+  let ll = 0;
   while (true) {
     //sammle daten
     numCracks = getNumCracks();
@@ -123,7 +116,6 @@ export async function main(ns) {
       curBots = newTargets;
     }
 
-    bringHomeTheBacon(curBots, "spamHack.js", piggyBank)
 
     //monitor vorbereiten
 
@@ -132,45 +124,23 @@ export async function main(ns) {
 
 
     ns.clearLog();
-    for (let bot of curBots) {
-      ns.print (`${bot} :: ${ns.formatNumber(ns.getServerMoneyAvailable(bot))}`);
-    }
     ns.print(" ");
-    let money = ns.getServerMoneyAvailable(piggyBank);
+    let money = ns.getServerMoneyAvailable(bestOption);
     if (money === 0) money = 1;
-    let maxMoney = ns.getServerMaxMoney(piggyBank);
-    let minSec = ns.getServerMinSecurityLevel(piggyBank);
-    let sec = ns.getServerSecurityLevel(piggyBank);
-    ns.print(`${piggyBank}:`);
-    ns.print(` $_______: ${ns.formatNumber(money)} / ${ns.formatNumber(maxMoney)} (${(money / maxMoney * 100).toFixed(2)}%)`);
-    ns.print(` security: +${(sec - minSec).toFixed(2)}`);
-    ns.print(` weaken__:  (t=${Math.ceil((sec - minSec) * 20)})`);
-    money = ns.getServerMoneyAvailable(bestOption);
-    if (money === 0) money = 1;
-    maxMoney = ns.getServerMaxMoney(bestOption);
-    minSec = ns.getServerMinSecurityLevel(bestOption);
-    sec = ns.getServerSecurityLevel(bestOption);
+    let maxMoney = ns.getServerMaxMoney(bestOption);
+    let minSec = ns.getServerMinSecurityLevel(bestOption);
+    let sec = ns.getServerSecurityLevel(bestOption);
     ns.print(" ");
     ns.print(`${bestOption}:`);
     ns.print(` $_______: ${ns.formatNumber(money)} / ${ns.formatNumber(maxMoney)} (${(money / maxMoney * 100).toFixed(2)}%)`);
     ns.print(` security: +${(sec - minSec).toFixed(2)}`);
     ns.print(` weaken__:  (t=${Math.ceil((sec - minSec) * 20)})`);
+    ll = (ll + .12); if (ll>=4.0) ll=0.0;
+    ns.print(`${lifeLine[Math.floor(ll)]}`);
 
     // etwas warten
-    await ns.hack(piggyBank);
+    //await ns.hack(bestOption);
     await ns.sleep(waitTime);
-  }
-
-  function bringHomeTheBacon(bots, script, target) {
-    for (let server of bots) {
-      ns.scp(script, server);
-
-      if (ns.scriptRunning(script, server)) {
-        ns.scriptKill(script, server);
-      }
-
-      ns.exec(script, server, 1, target);
-    }
   }
 
 }
---
+++ readme.md
@@ -0,0 +1,30 @@
+# egos Bitburner progress
+
+Welcome to my playground with Bitburner scripts. They are written in the game's own language Netscript, which is a subset of Javascript. 
+
+If you want to play the game yourself - click on one of the following links.
+[Bitburner (Github)](https://danielyxie.github.io/bitburner/) 
+[Bitburner (Steam)](https://store.steampowered.com/app/1812820/Bitburner/)
+
+What you see here is mostly copied from others
+
+## Requirements
+
+My script should be able to run with the 8 GB of RAM you have at game start. ** At least it does now that I'm writing this :) **
+
+## Instalation
+
+1. Load the script named `git-init.js` from GitHub by executing the following line in the Bitburner terminal:
+
+`wget https://raw.githubusercontent.com/ego-tsioge/bitburner-scripts/main/git-init.js git-init.js`
+
+2. Start th script with `run git-init.js` 
+
+This script automatically loads this repro onto the home server.
+
+# personal notes
+
+* https://bitburner.readthedocs.io/en/stable/
+
+repros that I would like to look into when the opportunity arises:
+* https://github.com/afsanchez001/BitburnerRepo for tables.js
