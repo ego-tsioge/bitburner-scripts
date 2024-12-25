0f148499681d4fedaae4341844ebbfb7f0bc0647
ego-tsioge <{ID}+{username}@users.noreply.github.com>
2024-12-25 15:24:52 +0100

    fix: reduziert auf leere Main

--- bitburner-gamefolder/scripts/mod.hacknet.js
+++ bitburner-gamefolder/scripts/mod.hacknet.js
@@ -2,148 +2,43 @@ import { SETTINGS } from '/scripts/lib.config.js';
 import { Logger } from '/scripts/lib.log.js';
 
 /**
- * Hacknet Node Manager
+ * Hacknet Server Manager
+ * Kauft und verwaltet Hacknet Server nach verschiedenen Strategien. In der Grundeinstellung 
+ * kauft es bis zur Einladungsvoraussetzung für die Faction:Netburners
+ * 
  * @param {NS} ns - Bitburner Netscript API
  */
 export async function main(ns) {
 	const flags = ns.flags([
-		['debug', SETTINGS.DEBUG],      // Debug-Modus
-		['mode', SETTINGS.HACKNET.MODE], // Betriebsmodus
+		['debug', SETTINGS.DEBUG],           // Debug-Modus
+		['mode', SETTINGS.HACKNET.MODE],     // Betriebsmodus
 		['budget', SETTINGS.HACKNET.BUDGET], // Budget überschreiben
-		['help', false]                 // Hilfe anzeigen
+		['help', false],                     // Hilfe anzeigen
+		['nodes', SETTINGS.HACKNET.NODES],   // Anzahl der Nodes
+		['level', SETTINGS.HACKNET.LEVEL],   // Minimale Summe an Level
+		['ram', SETTINGS.HACKNET.RAM],       // Minimale Summe an RAM
+		['cores', SETTINGS.HACKNET.CORES]    // Minimale Summe an Cores
 	]);
 	
 	if (flags.help) {
 		ns.tprint(`
-Verwendung: run ${ns.getScriptName()}
+Verwendung: run ${ns.getScriptName()} [flags] 
 Flags:
   --debug           Debug-Modus aktivieren
   --mode <mode>     Betriebsmodus (order|free|freeOverOrder)
   --budget <money>  Budget für Free-Mode
   --help            Diese Hilfe anzeigen
+  
+  diese gesamtzahl der elemente erreichen:
+  --nodes <number>  Anzahl der Nodes
+  --level <number>  Minimale Summe an Level
+  --ram <number>    Minimale Summe an RAM
+  --cores <number>  Minimale Summe an Cores
 		`);
 		return;
 	}
 
 	const log = new Logger(ns, ns.getScriptName(), { showDebug: flags.debug });
-	
-	try {
-		const manager = new HacknetManager(ns, log, {
-			mode: flags.mode,
-			budget: flags.budget
-		});
-		await manager.run();
-		
-		// Zurück zu basis.js
-		ns.spawn('basis.js', 1);
-	} catch (error) {
-		log.handleError(error, true);
-	}
+
+	// noch funktionslos
 }
-
-class HacknetManager {
-	constructor(ns, log, options = {}) {
-		this.ns = ns;
-		this.log = log;
-		this.hn = ns.hacknet;
-		this.config = {
-			...SETTINGS.HACKNET,
-			...options
-		};
-		this.budget = Math.max(this.config.budget, 0);
-	}
-
-	async run() {
-		// Erste Node kaufen falls keine vorhanden
-		if (this.hn.numNodes() === 0) {
-			this.hn.purchaseNode();
-		}
-
-		// Order Mode
-		if (this.config.mode === 'order' || this.config.mode === 'freeOverOrder') {
-			await this.runOrderMode();
-		}
-
-		// Free Mode
-		if (this.config.mode === 'free' || this.config.mode === 'freeOverOrder') {
-			await this.runFreeMode();
-		}
-	}
-
-	async runOrderMode() {
-		this.log.info('Starte Order Mode');
-		
-		// Nodes kaufen
-		while (this.hn.numNodes() < this.config.NODES) {
-			await this.purchaseOrWait(() => {
-				return this.hn.purchaseNode() !== -1;
-			}, this.hn.getPurchaseNodeCost());
-		}
-
-		// Bestandsaufnahme
-		const stats = this.calculateCurrentStats();
-		await this.upgradeToMinimumRequirements(stats);
-	}
-
-	calculateCurrentStats() {
-		let sumLevel = 0, sumRam = 0, sumCores = 0;
-		
-		for (let i = 0; i < this.hn.numNodes(); i++) {
-			const stats = this.hn.getNodeStats(i);
-			sumLevel += stats.level;
-			sumRam += stats.ram;
-			sumCores += stats.cores;
-		}
-
-		return {
-			neededLevel: Math.max(this.config.LEVEL - sumLevel, 0),
-			neededRam: Math.max(this.config.RAM - sumRam, 0),
-			neededCores: Math.max(this.config.CORES - sumCores, 0)
-		};
-	}
-
-	async purchaseOrWait(buyFunction, cost) {
-		if (buyFunction()) {
-			this.budget -= cost;
-			return true;
-		}
-		await this.ns.sleep(this.config.WAIT_TIME);
-		return false;
-	}
-
-	async upgradeToMinimumRequirements(stats) {
-		this.log.info('Upgrade auf Mindestanforderungen');
-		
-		for (let i = 0; i < this.hn.numNodes(); i++) {
-			// Level upgraden
-			while (stats.neededLevel > 0) {
-				const cost = this.hn.getLevelUpgradeCost(i, 1);
-				if (await this.purchaseOrWait(() => {
-					return this.hn.upgradeLevel(i, 1);
-				}, cost)) {
-					stats.neededLevel--;
-				}
-			}
-			
-			// RAM upgraden
-			while (stats.neededRam > 0) {
-				const cost = this.hn.getRamUpgradeCost(i, 1);
-				if (await this.purchaseOrWait(() => {
-					return this.hn.upgradeRam(i, 1);
-				}, cost)) {
-					stats.neededRam--;
-				}
-			}
-			
-			// Cores upgraden
-			while (stats.neededCores > 0) {
-				const cost = this.hn.getCoreUpgradeCost(i, 1);
-				if (await this.purchaseOrWait(() => {
-					return this.hn.upgradeCore(i, 1);
-				}, cost)) {
-					stats.neededCores--;
-				}
-			}
-		}
-	}
-} 
